fc831d91998f3fb8b0da58838423093fd83763cf
porori <charizard006@protonmail.com>
2022-06-01 15:32:30 +0900

    make real time chat func

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,19 +1,43 @@
 //백엔드랑 연결해달라고 해야함.
-
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickNameForm = document.querySelector("#nickname");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+
+const makeMessage = (type, payload) => {
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+const handleNickNameSubmit = event => {
+    event.preventDefault();
+    const input = nickNameForm.querySelector("input");
+    socket.send(makeMessage("nickName", input.value));
+}
+
+const handleMessageSubmit = event => {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server");
 })
+
 //백엔드에서 보낸 메세지 받는것.
 socket.addEventListener("message", (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
     console.log("Just got this: ", message.data, "from the server");
 })
 
 socket.addEventListener("close", () => {
     console.log("Disconnected to Server");
 })
-
-setTimeout(() => {
-    socket.send("Hello from the browser!");
-},10000)
+nickNameForm.addEventListener("submit", handleNickNameSubmit);
+messageForm.addEventListener("submit",handleMessageSubmit);
--- src/server.js
+++ src/server.js
@@ -18,12 +18,30 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new Websocket.Server({ server });
 
+
+const sockets = [];
+
+//아래코드는 브라우져가 다를 때 여러번 실행됨.
 wss.on("connection",(socket) => {
+
+    sockets.push(socket);
+    socket["nickName"] = "Annaymous";
     //socket 메세지로 브라우져로 보냄/ wss 메서드아님
     console.log("Connected to Browser");
     socket.on("close", () => console.log("Disconnected from the Browser"));
-    socket.on("message", (message) => console.log(message.toString('utf8')));
-    socket.send("hello!");
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        console.log(message);
+        
+        switch(message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickName}: ${message.payload.toString()}`));
+            case "nickName":
+                //socket은 객체기때문에 닉네임 누군지 넣어주면됨.
+                socket["nickName"] = message.payload;
+        }
+        //접속된 모든 소켓들에게 메세지 보내기
+    });
 });
 
 server.listen(3000, handleListen);
